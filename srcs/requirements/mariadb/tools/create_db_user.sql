f83b2ff932593cf8a75020607fb4a03338101120
tblaase <tblaase@student.42heilbronn.de>
2022-06-23 13:47:43 +0200

    fixed typo

--- srcs/requirements/mariadb/tools/create_db_user.sql
+++ srcs/requirements/mariadb/tools/create_db_user.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE wordpress;
 CREATE USER 'tblaase'@'%' IDENTIFIED BY 'root';
-GRANT ALL PRIVILIGES ON wordpress.* TO 'tblaase'@'%';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'tblaase'@'%';
 FLUSH PRIVILEGES;
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY '4242';
