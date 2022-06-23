a641d5e30004d28fc9c90526c4ac18f32f9c9285
tblaase <tblaase@student.42heilbronn.de>
2022-06-23 17:00:02 +0200

    changed to apt-get

--- inception_prep.sh
+++ inception_prep.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #this will prep a new installation of a virtual machine for inception
 #the VM it is tested with is Ubuntu 22.04 LTS 64 bit as minimal installation, a size of 20GB is reccomended
-sudo apt update
-sudo apt upgrade -y
-sudo apt install make curl lsb-release ca-certificates apt-transport-https software-properties-common -y
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install make curl lsb-release ca-certificates apt-transport-https software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce -y
-sudo apt update
-sudo apt install docker-compose-plugin -y
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo apt-get update
+sudo apt-get install docker-compose-plugin -y
