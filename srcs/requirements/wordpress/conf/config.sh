2357dcd9f207b7d2ccad1faaf924838060d73027
tblaase <tblaase@student.42heilbronn.de>
2022-06-23 14:43:40 +0200

    added worpress, now test

---
+++ srcs/requirements/wordpress/conf/config.sh
@@ -0,0 +1,19 @@
+cd /var/www/wordpress
+wp core config	--dbhost=$DB_HOST \
+				--dbname=$DB_NAME \
+				--dbuser=$DB_USER \
+				--dbpass=$DB_PASSWORD \
+				--allow-root
+
+wp core install --title=$WP_TITLE \
+				--admin_user=$WP_ADMIN_USER \
+				--admin_password=$WP_ADMIN_PASSWORD \
+				--admin_email=$WP_ADMIN_MAIL \
+				--url=$WP_URL \
+				--allow-root
+
+wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
+cd -
+
+# run php-fpm7.3 listening for CGI request and force to stay in foreground and ignore daemonize option fromm configuration file
+php-fpm7.3 -F
