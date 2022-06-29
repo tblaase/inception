94005ae2956b5ba72151fb12864c1a61c317a33b
tblaase <tblaase@student.42heilbronn.de>
2022-06-29 17:02:38 +0200

    now informes about creation of directories

--- inception_prep.sh
+++ inception_prep.sh
@@ -13,14 +13,17 @@ sudo apt-get install docker-compose-plugin -y
 if [ -d "/home/$USER/data" ]; then \
 	echo "/home/$USER/data already exists"; else \
 	mkdir /home/$USER/data; \
+	echo "data directory created successfully"; \
 fi
 
 if [ -d "/home/$USER/data/wordpress" ]; then \
 	echo "/home/$USER/data/wordpress already exists"; else \
 	mkdir /home/$USER/data/wordpress; \
+	echo "wordpress directory created successfully"; \
 fi
 
 if [ -d "/home/$USER/data/mariadb" ]; then \
 	echo "/home/$USER/data/mariadb already exists"; else \
 	mkdir /home/$USER/data/mariadb; \
+	echo "mariadb directory created successfully"; \
 fi
