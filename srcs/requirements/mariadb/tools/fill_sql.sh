e0f1c338f812db2d6b61b642c23ff326a00e4d04
tblaase <tblaase@student.42heilbronn.de>
2022-06-30 17:36:19 +0200

    renamed and revamped the script to create user for DB

--- srcs/requirements/mariadb/tools/fill_sql.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "CREATE DATABASE $DB_NAME;" >> create_db_user.sql
-echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> create_db_user.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" >> create_db_user.sql
-echo "FLUSH PRIVILEGES;" >> create_db_user.sql
-echo "ALTER USER 'root'@'%' IDENTIFIED BY '4242';" >> create_db_user.sql
